a81284fec0325142b5b67c223c7e594a08b55801
mehak-fabtechsol <mehak@fabtechsol.com>
2024-02-20 16:55:39 +0500

    welcome fixes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,9 +5,9 @@ function Welcome() {
   return (
    <section>
     <div className="mycontainer">
-      <div className="row">
-        <h1 className='mb-0'>You are loged in</h1>
-        <Link to="/" className="text-dark text-decoration-none">
+      <div className="d-flex py-4 align-items-center justify-content-between">
+        <p className='mb-0 fs-5'>You are loged in</p>
+        <Link to="/" className=" text-dark text-decoration-none py-2 d-block text-decoration-none text-center bg-black border-0  fw-md text-white rounded-3 py-2 px-4 ">
                     Logout
                   </Link>
       </div>
